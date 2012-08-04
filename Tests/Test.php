19720f61872129e6c3a7c558d43801d92f223152
Petit Yoann <zenyaku@free.fr>
2012-08-04 19:18:23 +0200

    Update for tag and travis

---
+++ Tests/Test.php
@@ -0,0 +1,10 @@
+<?php
+namespace APY\JsFormValidationBundle\Tests;
+
+class Test extends \PHPUnit_Framework_TestCase
+{
+    public function testPHPUnit()
+    {
+        $this->assertTrue(true);
+    }
+}
